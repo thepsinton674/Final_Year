e60b0d9e2bfe625a5f99dff08da0db9d27388d0f
Peter Sinton <petersinton16@gmail.com>
2018-06-05 19:26:03 +0100

    UE4
    All work from UE4 i could get by adding the repository from the GitHub desktop app.

---
+++ Source/Final_Year/Final_Year.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Final_Year.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Final_Year, "Final_Year" );
---
+++ Source/Final_Year/Final_YearGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Final_YearGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINAL_YEAR_API AFinal_YearGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
